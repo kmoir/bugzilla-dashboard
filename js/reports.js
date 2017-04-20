6b38faf383561eb287da98416d3b1da1351f357c
Chris Cooper <ccooper@deadsquid.com>
2017-04-20 10:27:44 -0400

    Split coop's reports into two functional groups

--- js/reports.js
+++ js/reports.js
@@ -5,33 +5,37 @@ var reports = {
              "mcornmesser@mozilla.com",
              "q@mozilla.com",
              "rthijssen@mozilla.com"
-           ],
+    ],
     'catlee': [
 		"aki@mozilla.com",
-                "bhearsum@mozilla.com",
-                "jlorenzo@mozilla.com",
-                "jlund@mozilla.com",
-                "kmoir@mozilla.com",
-		"nthomas@mozilla.com",
+        "bhearsum@mozilla.com",
+        "jlorenzo@mozilla.com",
+        "jlund@mozilla.com",
+        "kmoir@mozilla.com",
+        "nthomas@mozilla.com",
 		"rail@mozilla.com",
-                "sfraser@mozilla.com"
-		],
-    'coop': [
+        "sfraser@mozilla.com"
+    ],
+    'coop-build': [
+        "cmanchester@mozilla.com",
+        "gps@mozilla.com",
+        "mshal@mozilla.com",
+        "ted@mielczarek.org"
+    ],
+    'coop-sheriffs': [
              "cbook@mozilla.com",
-             "cmanchester@mozilla.com",
-             "gps@mozilla.com",
              "ihsiao@mozilla.com",
-             "mshal@mozilla.com",
-             "ted@mielczarek.org",
-             "wkocher@mozilla.com"
-            ],
+             "wkocher@mozilla.com",
+             "philringnalda@gmail.com",
+             "aryx.bugmail@gmx-topmail.de"
+    ],
     'interns': [
                 "csheehan@mozilla.com",
                 "btang@mozilla.com"
-               ],
+    ],
     'jlund': [
              "bugspam.Callek@gmail.com",
              "mtabara@mozilla.com",
              "rgarbas@mozilla.com"
-	     ],
+    ],
 };
