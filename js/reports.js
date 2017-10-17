903280ac6043ea82987d5e1eddd55cc66d837f23
Chris Cooper <ccooper@deadsquid.com>
2017-10-17 09:43:38 -0400

    Make coop's display the default

--- js/reports.js
+++ js/reports.js
@@ -1,37 +1,14 @@
 var reports = {
-    'arr': [
-             "klibby@mozilla.com",
-             "jwatkins@mozilla.com",
-             "mcornmesser@mozilla.com",
-             "q@mozilla.com",
-             "rthijssen@mozilla.com"
-    ],
-    'catlee': [
-		"aki@mozilla.com",
-        "bhearsum@mozilla.com",
-        "jlorenzo@mozilla.com",
-        "jlund@mozilla.com",
-        "kmoir@mozilla.com",
-        "nthomas@mozilla.com",
-		"rail@mozilla.com",
-        "sfraser@mozilla.com"
-    ],
     'coop-build': [
+        "core-build-config-reviews@mozilla.bugs",
         "cmanchester@mozilla.com",
         "gps@mozilla.com",
         "mshal@mozilla.com",
         "ted@mielczarek.org"
     ],
     'coop-sheriffs': [
-             "cbook@mozilla.com",
-             "ihsiao@mozilla.com",
-             "wkocher@mozilla.com",
              "philringnalda@gmail.com",
              "aryx.bugmail@gmx-topmail.de"
-    ],
-    'interns': [
-                "csheehan@mozilla.com",
-                "btang@mozilla.com"
     ],
     'jlund': [
              "bugspam.Callek@gmail.com",
